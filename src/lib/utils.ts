9978aba130ec2f06492c79711ca911ed2d280e25
abhinandan-verma <abhinandanverma551@gmail.com>
2024-06-05 03:37:29 +0530

    summary in progress

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,3 +23,15 @@ export function formatCurrency(value: number){
     minimumFractionDigits: 2,
   }).format(value);
 }
+
+
+export function calculatePercentageChange(
+  current: number,
+  previous: number
+){
+  if(previous === 0){
+    return previous == current ? 0 : 100
+  }
+
+  return ((current - previous)/previous ) * 100;
+}
